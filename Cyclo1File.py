a2d6a84d9afdab1a68eee6397a7ac276f3222a17
MAIHfly <mhosfeld@andrew.cmu.edu>
2022-04-05 14:38:33 -0400

    change of inputs to bBiCy

--- Cyclo1File.py
+++ Cyclo1File.py
@@ -8,7 +8,7 @@ ade.Config.n_cores = Cores
 orca = ade.methods.ORCA()
 xtb = ade.methods.XTB()
 
-MoI1 = ade.Molecule('aICyDNH_New.xyz', solvent_name='toluene', charge=0)
+MoI1 = ade.Molecule('bBiCyDNH_New.xyz', solvent_name='toluene', charge=0)
 
 print(MoI1, flush=True)
 
