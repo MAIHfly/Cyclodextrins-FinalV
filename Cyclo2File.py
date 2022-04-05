e18a393308d0c3dad3efd4f9db986bc44eac7336
MAIHfly <mhosfeld@andrew.cmu.edu>
2022-04-05 06:28:20 -0400

    change of inputs to aI45Me2Cy

--- Cyclo2File.py
+++ Cyclo2File.py
@@ -8,7 +8,7 @@ ade.Config.n_cores = Cores
 orca = ade.methods.ORCA()
 xtb = ade.methods.XTB()
 
-MoI2 = ade.Molecule('aBiCyDNH2_New.xyz', solvent_name='DCM', charge=1)
+MoI2 = ade.Molecule('aI45Me2CyDNH2_New.xyz', solvent_name='DCM', charge=1)
 
 print(MoI2, flush=True)
 
